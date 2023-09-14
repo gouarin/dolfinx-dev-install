7c3bec6d2b1d2d0b30dfe9e4750ccd31fa88e04b
Loic Gouarin <loic.gouarin@gmail.com>
2023-09-14 16:16:55 +0200

    fix versions and update dependencies

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ mkdir src
 
 git clone https://github.com/FEniCS/basix.git src/basix
 pushd src/basix
+git checkout 0fc130f7d790f793ffa57bf8d056232ff6f12be8
 cmake \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
     -B build-dir \
     -DCMAKE_INSTALL_PREFIX="$CONDA_PREFIX" \
-    -DBLAS_VENDOR="Generic" \
-    -DBLAS_LIBRARIES="$CONDA_PREFIX/lib/libblas${SHLIB_EXT};$CONDA_PREFIX/lib/libcblas${SHLIB_EXT}" \
     cpp
 cmake --build build-dir
 cmake --install build-dir
@@ -19,6 +18,7 @@ popd
 
 git clone https://github.com/FEniCS/ffcx.git src/ffcx
 pushd src/ffcx
+git checkout 237b6135912ba3a7f87c7d2c7face87245cd8a76
 cmake \
     -DCMAKE_INSTALL_PREFIX="${CONDA_PREFIX}" \
     -B build-dir \
@@ -30,6 +30,7 @@ popd
 
 git clone https://github.com/FEniCS/dolfinx.git src/dolfinx
 pushd src/dolfinx
+git checkout aa4242a3319de0fffa34e64c45afb059cbdabea4
 cmake \
     -DDOLFINX_UFCX_PYTHON=OFF \
     -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
